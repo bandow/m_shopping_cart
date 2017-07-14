92799f48ed497b46d278ea4de0f3c9ace1305950
bandow <sycbbb@sina.com>
2017-07-14 18:13:41 +0800

    cart

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+var app=new Vue({
+	el:"#app",
+	data:{
+		list:new Array(),
+	},
+	mounted:function(){
+		var _this=this;
+		this.$nextTick(function(){
+			axios.get('http://10.44.172.245:3305/data/data.json')
+			.then(
+				function(retObj){
+					if(retObj.status == 200){
+						app.list = retObj.data.result.list;
+					}
+				}
+			)
+			.catch(function(errorObj){
+				console.log("get data error...");
+			})
+		});
+
+	},
+	methods:{
+	},
+	watch:{
+	},
+	computed:{	
+	},
+	filters:{
+	}
+})
